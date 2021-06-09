7f753105dfc254cb1f49cd5fe4f318306f690730
TiPu SuLtAn <bdsoldire@gmail.com>
2021-06-10 00:19:52 +0600

    react-form

---
+++ src/FormSuccess.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Form.css";
+
+const FormSuccess = () => {
+  return (
+    <div className="form-content-right">
+      <h1 className="form-success">We have received your request!</h1>
+      <img src="img/img-2.svg" alt="success" className="form-img-2" />
+    </div>
+  );
+};
+
+export default FormSuccess;
